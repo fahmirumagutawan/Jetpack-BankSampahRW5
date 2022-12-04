c72c86a6b9d816a191498517a3e3148a00f14ba6
fahmirumagutawan <fahmigutawan@gmail.com>
2022-12-05 06:04:07 +0700

    Some additional initial commit

--- core/src/main/java/com/filkom/core/data/source/firebase/FirebaseDataSource.kt
+++ core/src/main/java/com/filkom/core/data/source/firebase/FirebaseDataSource.kt
@@ -1,4 +1,13 @@
 package com.filkom.core.data.source.firebase
 
-class FirebaseDataSource {
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.storage.FirebaseStorage
+import javax.inject.Inject
+
+class FirebaseDataSource @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+    private val firebaseStorage: FirebaseStorage,
+    private val firebaseMessaging: FirebaseMessaging
+) {
 }
--- core/src/main/java/com/filkom/core/di/FirebaseModule.kt
+++ core/src/main/java/com/filkom/core/di/FirebaseModule.kt
@@ -1,6 +1,9 @@
 package com.filkom.core.di
 
 import com.filkom.core.data.source.firebase.FirebaseDataSource
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,5 +13,22 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 class FirebaseModule {
     @Provides
-    fun provideFirebaseSource() = FirebaseDataSource()
+    fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+
+    @Provides
+    fun provideFirebaseStorage() = FirebaseStorage.getInstance()
+
+    @Provides
+    fun provideFirebaseMessaging() = FirebaseMessaging.getInstance()
+
+    @Provides
+    fun provideFirebaseSource(
+        firebaseAuth:FirebaseAuth,
+        firebaseStorage: FirebaseStorage,
+        firebaseMessaging: FirebaseMessaging
+    ) = FirebaseDataSource(
+        firebaseAuth = firebaseAuth,
+        firebaseStorage = firebaseStorage,
+        firebaseMessaging = firebaseMessaging
+    )
 }
