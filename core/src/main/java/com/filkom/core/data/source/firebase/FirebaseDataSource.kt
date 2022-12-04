a87c8644219c5edce98a35943f3d8bbb656f2a2f
fahmirumagutawan <fahmigutawan@gmail.com>
2022-12-05 06:19:35 +0700

    Some additional initial commit

--- core/src/main/java/com/filkom/core/data/source/firebase/FirebaseDataSource.kt
+++ core/src/main/java/com/filkom/core/data/source/firebase/FirebaseDataSource.kt
@@ -1,5 +1,6 @@
 package com.filkom.core.data.source.firebase
 
+import com.filkom.core.util.DelimaException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.storage.FirebaseStorage
@@ -10,4 +11,27 @@ class FirebaseDataSource @Inject constructor(
     private val firebaseStorage: FirebaseStorage,
     private val firebaseMessaging: FirebaseMessaging
 ) {
+    /*Login with email and password*/
+    fun loginWithEmailAndPassword(
+        email:String,
+        password:String,
+        onSuccess:() -> Unit,
+        onFailed:(DelimaException) -> Unit
+    ){
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailed(DelimaException(it.message)) }
+    }
+
+    /*Sign up with email and password*/
+    fun signUpWithEmailAndPassword(
+        email:String,
+        password:String,
+        onSuccess:() -> Unit,
+        onFailed:(DelimaException) -> Unit
+    ){
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onFailed(DelimaException(it.message)) }
+    }
 }
